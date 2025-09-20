7cc7b1bfc660094c247572caee6ee19926e0729a
Adam Soták <92621643+AdamSotak@users.noreply.github.com>
2025-09-20 17:23:48 +0200

    Initial commit

---
+++ resources/js/app.tsx
@@ -0,0 +1,25 @@
+/** biome-ignore-all lint/suspicious/noExplicitAny: Not typed */
+
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+import Layout from "./layouts/layout";
+
+createInertiaApp({
+	resolve: (name) => {
+		const pages = (import.meta as any).glob("./pages/**/*.tsx", {
+			eager: true,
+		}) as Record<string, { default: React.ComponentType<any> }>;
+		const page = pages[`./pages/${name}.tsx`];
+		if (!page) {
+			throw new Error(`Page not found: ${name}`);
+		}
+		return page.default;
+	},
+	setup({ el, App, props }) {
+		createRoot(el).render(
+			<Layout>
+				<App {...props} />
+			</Layout>,
+		);
+	},
+});
