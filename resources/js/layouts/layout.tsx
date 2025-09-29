9a5aea59de74b99c5c100069b2f8cbe3323f1513
Adam Soták <92621643+AdamSotak@users.noreply.github.com>
2025-09-29 23:13:11 +0200

    feat: Navbar UI (#5)
    * add: Setup packages and environment tools
    
    * add: Svg icons
    
    * add: Update central app files
    
    * add: Setup shadcn components
    
    * add: Create login and signup pages
    
    * fix: Remove semicolons
    
    * add: Setup packages
    
    * add: Navbar UI
    
    * feat: Auth UI (#1)
    
    * add: Setup packages and environment tools
    
    * add: Svg icons
    
    * add: Update central app files
    
    * add: Setup shadcn components
    
    * add: Create login and signup pages
    
    * fix: Remove semicolons

--- resources/js/layouts/layout.tsx
+++ resources/js/layouts/layout.tsx
@@ -1,3 +1,10 @@
+import Navbar from "@/components/navbar"
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-	return <>{children}</>;
+	return (
+		<div className="min-w-screen min-h-screen bg-black">
+			<Navbar />
+			{children}
+		</div>
+	)
 }
