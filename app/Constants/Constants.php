b03b8ffd7c628e5b6f8c4b00711ba53d115647c1
Adam Soták <92621643+AdamSotak@users.noreply.github.com>
2025-09-29 23:19:30 +0200

    feat: Home sections UI (#7)
    * add: Setup packages and environment tools
    
    * add: Svg icons
    
    * add: Update central app files
    
    * add: Setup shadcn components
    
    * add: Create login and signup pages
    
    * fix: Remove semicolons
    
    * add: Setup packages
    
    * add: Navbar UI
    
    * add: Setup packages
    
    * add: New shadcn components
    
    * add: Update Navbar
    
    * add: Create responsive audio player UI
    
    * fix: Ignore layout on login and signup pages
    
    * add: Setup packages
    
    * fix: Update linter config
    
    * add: Image assets
    
    * add: Categories constant
    
    * add: Switch to Laravel controllers
    
    * fix: Hide all scrollbars
    
    * add: New shadcn components
    
    * add: Define types
    
    * add: Modals
    
    * add: Categories
    
    * add: Home sections UI

---
+++ app/Constants/Constants.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Constants;
+
+class Constants
+{
+    public const CATEGORIES = [
+        [
+            'id' => 'summer',
+            'name' => 'Summer',
+            'image' => 'images/categories/summer.jpg',
+            'color' => '#27856a',
+        ],
+        [
+            'id' => 'pop',
+            'name' => 'Pop',
+            'image' => 'images/categories/pop.jpg',
+            'color' => '#477d95',
+        ],
+        [
+            'id' => 'hip-hop',
+            'name' => 'Hip-Hop',
+            'image' => 'images/categories/hip-hop.jpg',
+            'color' => '#477d95',
+        ],
+        [
+            'id' => 'rock',
+            'name' => 'Rock',
+            'image' => 'images/categories/rock.jpg',
+            'color' => '#006450',
+        ],
+        [
+            'id' => 'latin',
+            'name' => 'Latin',
+            'image' => 'images/categories/latin.jpg',
+            'color' => '#0d73ec',
+        ],
+        [
+            'id' => 'dance-electronic',
+            'name' => 'Dance/Electronic',
+            'image' => 'images/categories/dance-electronic.jpg',
+            'color' => '#e61e32',
+        ],
+        [
+            'id' => 'mood',
+            'name' => 'Mood',
+            'image' => 'images/categories/mood.jpg',
+            'color' => '#e1118c',
+        ],
+        [
+            'id' => 'workout',
+            'name' => 'Workout',
+            'image' => 'images/categories/workout.jpg',
+            'color' => '#777777',
+        ],
+        [
+            'id' => 'country',
+            'name' => 'Country',
+            'image' => 'images/categories/country.jpg',
+            'color' => '#d84000',
+        ],
+        [
+            'id' => 'k-pop',
+            'name' => 'K-pop',
+            'image' => 'images/categories/k-pop.jpg',
+            'color' => '#e61e32',
+        ],
+        [
+            'id' => 'chill',
+            'name' => 'Chill',
+            'image' => 'images/categories/chill.jpg',
+            'color' => '#b06239',
+        ],
+        [
+            'id' => 'sleep',
+            'name' => 'Sleep',
+            'image' => 'images/categories/sleep.jpg',
+            'color' => '#b06239',
+        ],
+        [
+            'id' => 'party',
+            'name' => 'Party',
+            'image' => 'images/categories/party.jpg',
+            'color' => '#b06239',
+        ],
+        [
+            'id' => 'focus',
+            'name' => 'Focus',
+            'image' => 'images/categories/focus.jpg',
+            'color' => '#b06239',
+        ],
+        [
+            'id' => 'metal',
+            'name' => 'Metal',
+            'image' => 'images/categories/metal.jpg',
+            'color' => '#e91429',
+        ],
+        [
+            'id' => 'jazz',
+            'name' => 'Jazz',
+            'image' => 'images/categories/jazz.jpg',
+            'color' => '#8d67ab',
+        ],
+        [
+            'id' => 'classical',
+            'name' => 'Classical',
+            'image' => 'images/categories/classical.jpg',
+            'color' => '#7d4b32',
+        ],
+        [
+            'id' => 'folk-and-acoustic',
+            'name' => 'Folk & Acoustic',
+            'image' => 'images/categories/folk-and-acoustic.jpg',
+            'color' => '#bc5900',
+        ],
+        [
+            'id' => 'soul',
+            'name' => 'Soul',
+            'image' => 'images/categories/soul.jpg',
+            'color' => '#dc148c',
+        ],
+        [
+            'id' => 'instrumental',
+            'name' => 'Instrumental',
+            'image' => 'images/categories/instrumental.jpg',
+            'color' => '#dc148c',
+        ],
+        [
+            'id' => 'punk',
+            'name' => 'Punk',
+            'image' => 'images/categories/punk.jpg',
+            'color' => '#e61e32',
+        ],
+        [
+            'id' => 'ambient',
+            'name' => 'Ambient',
+            'image' => 'images/categories/ambient.jpg',
+            'color' => '#148a08',
+        ],
+        [
+            'id' => 'blues',
+            'name' => 'Blues',
+            'image' => 'images/categories/blues.jpg',
+            'color' => '#0d73ec',
+        ],
+        [
+            'id' => 'wellness',
+            'name' => 'Wellness',
+            'image' => 'images/categories/wellness.jpg',
+            'color' => '#148a08',
+        ],
+        [
+            'id' => 'travel',
+            'name' => 'Travel',
+            'image' => 'images/categories/travel.jpg',
+            'color' => '#0d72ed',
+        ],
+        [
+            'id' => 'funk-and-disco',
+            'name' => 'Funk & Disco',
+            'image' => 'images/categories/funk-and-disco.jpg',
+            'color' => '#af2896',
+        ],
+    ];
+}
---
+++ resources/js/types.ts
@@ -0,0 +1,12 @@
+export type Category = {
+	id: string
+	name: string
+	image: string
+	color: string
+}
+
+export type Playlist = {
+	id: string
+	name: string
+	description: string
+}
